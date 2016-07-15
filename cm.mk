e8e034725da70f7b86bc3a4ee6e36eb30a61ccf2
Louis Popi <theh2o64@gmail.com>
2016-07-15 13:17:56 +0200

    h830: Update fingerprint to latest v10d
    ro.lge.swversion_arb=ARB01
    ro.lge.swversion=H83010d
    ro.lge.swversion_short=V10d
    ro.lge.swversion_rev=0
    ro.lge.factoryversion=LGH830AT-00-V10d-TMO-US-MAY-23-2016-ARB01+0

--- cm.mk
+++ cm.mk
@@ -14,5 +14,5 @@ PRODUCT_NAME := cm_h830
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g5" \
     PRODUCT_NAME="h1_tmo_us" \
-    BUILD_FINGERPRINT="lge/h1_tmo_us/h1:6.0.1/MMB29M/1606419497545:user/release-keys" \
-    PRIVATE_BUILD_DESC="h1_tmo_us-user 6.0.1 MMB29M 1606419497545 release-keys"
+    BUILD_FINGERPRINT="lge/h1_tmo_us/h1:6.0.1/MMB29M/1614410155f20:user/release-keys" \
+    PRIVATE_BUILD_DESC="h1_tmo_us-user 6.0.1 MMB29M 1614410155f20 release-keys"
