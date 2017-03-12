249bcedb6b3f939283974ec1261fc26da8d82247
Rashed Abdel-Tawab <rashed@linux.com>
2017-03-12 19:16:37 -0400

    h918: Remove useless overlay
    This is present in msm8996-common
    
    Change-Id: I8e786345be184ef405e73223c91830be62460ef2

--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,9 +26,6 @@ $(call inherit-product, device/lge/v20-common/v20.mk)
 # Bluetooth
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
-
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Sensors
 PRODUCT_COPY_FILES += \
