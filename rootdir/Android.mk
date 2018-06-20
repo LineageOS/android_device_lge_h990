4bc4ca22d0ba58e00bc1b1c2efb742a83ac88430
Eric Meddaugh <eric@meddaughs.com>
2018-06-20 15:12:20 -0400

    h990: Add dual sim changes
    * h990ds blob update
    * Kernel boot command line
    * Adjust build.prop for dual sim
    
    Change-Id: I3d1f1c66a87840e4d63515b8269bd0b41101b89a

---
+++ rootdir/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# Device init scripts
+include $(CLEAR_VARS)
+LOCAL_MODULE       := rild2.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/rild2.rc
+LOCAL_VENDOR_MODULE    := true
+LOCAL_MODULE_RELATIVE_PATH := init
+include $(BUILD_PREBUILT)
