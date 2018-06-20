072b92869d3a194b43ee0aaf720514e16a034db6
Eric Meddaugh <eric@meddaughs.com>
2018-06-20 15:12:20 -0400

    h990: Add dual sim changes
    * h990ds blob update
    * Kernel boot command line
    * Adjust build.prop for dual sim
    * Add flags for ril dual sim
    
    Change-Id: I3d1f1c66a87840e4d63515b8269bd0b41101b89a

--- device.mk
+++ device.mk
@@ -30,3 +30,10 @@ PRODUCT_COPY_FILES += \
 # WiFi Calibration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd.cal
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.multisim.config=dsds
+
+LOCAL_CFLAGS += \
+   -DANDROID_MULTI_SIM \
+   -DANDROID_SIM_COUNT_2
