25497bc886d19f5be9df9bd17a0cb85eae221959
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-16 10:27:06 -0700

    h990: Move device specific props to vendor
    Change-Id: I8b69a270ec3bfdd8d54e045c2b8b30f371c4e99a

--- device.mk
+++ device.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lge/h990/h990-vendor.mk)
+
+# Properties
+-include $(LOCAL_PATH)/vendor_prop.mk
 
 # common v20
 $(call inherit-product, device/lge/v20-common/v20.mk)
