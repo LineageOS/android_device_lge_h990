5313c22363aa81988292eceff77371d996bf7f9e
Alessandro Astone <ales.astone@gmail.com>
2020-10-29 08:40:54 +0100

    h990: Define a soong namespace
    Change-Id: Ida759ee1786c667e766da29103f0ecb54950c104

--- device.mk
+++ device.mk
@@ -33,6 +33,10 @@ $(call inherit-product, device/lge/v20-common/v20.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_elsa_global_com.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_variable.conf
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # WiFi Calibration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd.cal
