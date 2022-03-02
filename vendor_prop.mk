06bdfccb17176ca9780d0128024a70531e1fad48
Alexander Koskovich <akoskovich@pm.me>
2022-03-02 13:41:26 -0700

    h990: Set USB product string to 'LG V20'
    Change-Id: Idda062aaa38e16d7ba65356dbca8b467fbff14c1

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -6,3 +6,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=9 \
     telephony.lteOnCdmaDevice=1
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.usb.product_string=LG V20
