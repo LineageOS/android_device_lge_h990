1be36e6ded3adb20bc357a2d6e35240ce6280c33
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    h990: Adjust override prop key names
    Change-Id: I753a79af0150c7a302bd9bb698d86f9d957e7a84

--- lineage_h990.mk
+++ lineage_h990.mk
@@ -32,9 +32,7 @@ PRODUCT_MODEL := LG-H990
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="elsa" \
-    PRODUCT_DEVICE="elsa" \
-    PRODUCT_NAME="elsa_global_com" \
-    PRIVATE_BUILD_DESC="elsa_global_com-user 8.0.0 OPR1.170623.032 1832617289f73 release-keys"
-
-BUILD_FINGERPRINT := "lge/elsa_global_com/elsa:8.0.0/OPR1.170623.032/1832617289f73:user/release-keys"
+    BuildDesc="elsa_global_com-user 8.0.0 OPR1.170623.032 1832617289f73 release-keys" \
+    BuildFingerprint="lge/elsa_global_com/elsa:8.0.0/OPR1.170623.032/1832617289f73:user/release-keys" \
+    DeviceName="elsa_global_com" \
+    DeviceProduct="elsa"
